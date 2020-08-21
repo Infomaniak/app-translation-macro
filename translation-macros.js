8cc654920bf703ab5f055d2338ae04589cef79b4
Philippe Weidmann <philippe.weidmann@infomaniak.com>
2020-08-21 11:24:42 +0200

    Make key available on one platform

--- translation-macros.js
+++ translation-macros.js
@@ -17,29 +17,26 @@
 
     for (var i = 2; i < 1000; i += 1) {
         var key = translationSheet.GetRange("A" + i).GetValue();
+        var availability = translationSheet.GetRange("B" + i).GetValue();
+
         if (key.localeCompare("")) {
-            var result = generateKey(translationSheet, "B", i, key);
+            var result = generateKey(translationSheet, "C", i, key, availability);
             ouptutFrIos += result[0];
             ouptutFrAndroid += result[1];
 
-
-            rowValue = translationSheet.GetRange("C" + i).GetValue().replace(/'/g, "’");
-            result = generateKey(translationSheet, "C", i, key);
+            result = generateKey(translationSheet, "D", i, key, availability);
             ouptutDeIos += result[0];
             ouptutDeAndroid += result[1];
 
-            rowValue = translationSheet.GetRange("D" + i).GetValue().replace(/'/g, "’");
-            result = generateKey(translationSheet, "D", i, key);
+            result = generateKey(translationSheet, "E", i, key, availability);
             ouptutEnIos += result[0];
             ouptutEnAndroid += result[1];
 
-            rowValue = translationSheet.GetRange("E" + i).GetValue().replace(/'/g, "’");
-            result = generateKey(translationSheet, "E", i, key);
+            result = generateKey(translationSheet, "F", i, key, availability);
             ouptutItIos += result[0];
             ouptutItAndroid += result[1];
 
-            rowValue = translationSheet.GetRange("F" + i).GetValue().replace(/'/g, "’");
-            result = generateKey(translationSheet, "F", i, key);
+            result = generateKey(translationSheet, "G", i, key, availability);
             ouptutEsIos += result[0];
             ouptutEsAndroid += result[1];
 
@@ -65,43 +62,51 @@
     resultSheetAndroid.GetRange("D2").SetValue(ouptutItAndroid);
     resultSheetAndroid.GetRange("E2").SetValue(ouptutEsAndroid);
 
-    function generateKey(sheet, column, row, key) {
+    function generateKey(sheet, column, row, key, availability) {
         var rowValue = sheet.GetRange(column + row).GetValue().replace(/'/g, "’");
         var resultIos = "";
         var resultAndroid = "";
         if (rowValue.includes("</b>")) {
-            resultIos = '"' + key + '"' + " = " + '"<html><head><meta charset=\'utf-8\'></head>' + rowValue + '</html>";\n';
-            if (key.includes("##{")) {
-                if (key.includes("##{one}")) {
-                    resultAndroid = '<plurals name="' + key.replace("##{one}", "") + '">\n';
-                    resultAndroid += '<item quantity="one">![CDATA[' + rowValue + ']]></item>\n';
-                    var nextKey = sheet.GetRange("A" + (i + 1)).GetValue();
+            if (availability.includes("i")) {
+                resultIos = '"' + key + '"' + " = " + '"<html><head><meta charset=\'utf-8\'></head>' + rowValue + '</html>";\n';
+            }
+            if (availability.includes("a")) {
+                if (key.includes("##{")) {
+                    if (key.includes("##{one}")) {
+                        resultAndroid = '<plurals name="' + key.replace("##{one}", "") + '">\n';
+                        resultAndroid += '<item quantity="one">![CDATA[' + rowValue + ']]></item>\n';
+                        var nextKey = sheet.GetRange("A" + (i + 1)).GetValue();
 
-                    if (nextKey.includes("##{other}")) {
-                        var nextRowValue = sheet.GetRange(column + (row + 1)).GetValue().replace(/'/g, "’");
-                        resultAndroid += '<item quantity="other">![CDATA[' + nextRowValue + ']]></item>\n';
+                        if (nextKey.includes("##{other}")) {
+                            var nextRowValue = sheet.GetRange(column + (row + 1)).GetValue().replace(/'/g, "’");
+                            resultAndroid += '<item quantity="other">![CDATA[' + nextRowValue + ']]></item>\n';
+                        }
+                        resultAndroid += '</plurals>\n';
                     }
-                    resultAndroid += '</plurals>\n';
+                } else {
+                    resultAndroid = '\t<string name="' + key + '"><![CDATA[' + rowValue + ']]></string>\n';
                 }
-            } else {
-                resultAndroid = '\t<string name="' + key + '"><![CDATA[' + rowValue + ']]></string>\n';
             }
         } else {
-            resultIos = '"' + key + '"' + " = " + '"' + rowValue + '";\n';
-            if (key.includes("##{")) {
-                if (key.includes("##{one}")) {
-                    resultAndroid = '<plurals name="' + key.replace("##{one}", "") + '">\n';
-                    resultAndroid += '<item quantity="one">' + rowValue + '</item>\n';
-                    var nextKey = sheet.GetRange("A" + (i + 1)).GetValue();
+            if (availability.includes("i")) {
+                resultIos = '"' + key + '"' + " = " + '"' + rowValue + '";\n';
+            }
+            if (availability.includes("a")) {
+                if (key.includes("##{")) {
+                    if (key.includes("##{one}")) {
+                        resultAndroid = '<plurals name="' + key.replace("##{one}", "") + '">\n';
+                        resultAndroid += '<item quantity="one">' + rowValue + '</item>\n';
+                        var nextKey = sheet.GetRange("A" + (i + 1)).GetValue();
 
-                    if (nextKey.includes("##{other}")) {
-                        var nextRowValue = sheet.GetRange(column + (row + 1)).GetValue().replace(/'/g, "’");
-                        resultAndroid += '<item quantity="other">' + nextRowValue + '</item>\n';
+                        if (nextKey.includes("##{other}")) {
+                            var nextRowValue = sheet.GetRange(column + (row + 1)).GetValue().replace(/'/g, "’");
+                            resultAndroid += '<item quantity="other">' + nextRowValue + '</item>\n';
+                        }
+                        resultAndroid += '</plurals>\n';
                     }
-                    resultAndroid += '</plurals>\n';
+                } else {
+                    resultAndroid = '\t<string name="' + key + '">' + rowValue + '</string>\n';
                 }
-            } else {
-                resultAndroid = '\t<string name="' + key + '">' + rowValue + '</string>\n';
             }
         }
         return [resultIos, resultAndroid];
